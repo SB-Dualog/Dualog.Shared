6cb3280ff69501bb8b144439572b71c01044205c
alexanderhanssen <alexhsn1@gmail.com>
2017-08-16 12:46:09 +0200

    Adds DeviceInformationResponse

---
+++ Dualog.Shared/Api/DeviceInformationResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace Dualog.Shared.Api
+{
+    public class DeviceInformationResponse
+    {
+        public string CurrentAppVersion { get; set; }
+        public bool ClientVersionIsOutDated { get; set; }
+    }
+}
