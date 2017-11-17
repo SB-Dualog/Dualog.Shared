b57a6c43fb0a4d246dcfe88a35d29488c84904ab
alexanderhanssen <alexhsn1@gmail.com>
2017-11-17 15:08:07 +0100

    Should be able to get error translations

---
+++ Dualog.Shared/Models/ReturnMessageError.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dualog.Shared.Models
+{
+    public class ReturnMessageError
+    {
+        public ReturnMessageError(string code, string norwegian, string english)
+        {
+            Code = code;
+            NorwegianText = norwegian;
+            EnglishText = english;
+        }
+        public string Code { get; set; }
+        public string NorwegianText { get; set; }
+        public string EnglishText { get; set; }
+    }
+}
