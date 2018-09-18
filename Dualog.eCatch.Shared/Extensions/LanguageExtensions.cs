7c2b25a5a126be802242d014f7e833929a18c877
alexanderhanssen <alexhsn1@gmail.com>
2018-09-18 12:16:34 +0200

    Adds translations and support for translating

--- Dualog.eCatch.Shared/Extensions/LanguageExtensions.cs
+++ Dualog.eCatch.Shared/Extensions/LanguageExtensions.cs
@@ -1,11 +1,20 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Globalization;
+using System.Linq;
 using Dualog.eCatch.Shared.Enums;
+using Dualog.eCatch.Shared.Language;
 
 namespace Dualog.eCatch.Shared.Extensions
 {
     public static class LanguageExtensions
     {
+        public static string Translate(this string text, EcatchLangauge lang, bool isPlural = false)
+        {
+            var localizedString = Translations.ResourceManager.GetString(text, lang.ToUiCulture());
+            return ReplacePlurals(localizedString, isPlural) ?? text;
+        }
+
         public static CultureInfo ToUiCulture(this EcatchLangauge lang)
         {
             switch (lang)
@@ -24,6 +33,37 @@ namespace Dualog.eCatch.Shared.Extensions
             if (currentInfo.Name == "US" || currentInfo.Name == "GB") return EcatchLangauge.English;
 
             throw new ArgumentException($"{currentInfo.DisplayName} is not supported for localization of Dualog eCatch sources");
+        }
+
+        private static string ReplacePlurals(string localizedString, bool isPlural)
+        {
+            if (string.IsNullOrEmpty(localizedString))
+                return null;
+            if (!localizedString.Contains("|"))
+                return localizedString;
+            const string escapedpipe = "<escapedPipe>";
+            var escapedString = localizedString.Replace("||", escapedpipe); //Save escaped pipes and put them back in when done.
+            var words = escapedString.Split();
+            var pluralMap = MapPlurals(words, isPlural);
+            var finalString = pluralMap
+                .Aggregate(escapedString, (current, pair) => current.Replace(pair.Key, pair.Value))
+                .Replace(escapedpipe, "|");
+            return finalString;
+        }
+
+        private static Dictionary<string, string> MapPlurals(IEnumerable<string> words, bool isPlural)
+        {
+            var pluralMap = words.Where(w => w.Contains("|")).ToDictionary(w => w, w => {
+                var parts = w.Split('|');
+                if (parts.Length == 1)
+                {
+                    return parts[0];
+                }
+                var singular = parts[0];
+                var plural = parts[1];
+                return isPlural ? plural : singular;
+            });
+            return pluralMap;
         }
     }
 }
