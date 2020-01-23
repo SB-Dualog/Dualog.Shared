cbd628b0a4b713e3c8970a347292310b082548b0
vebjornhaugland <vebjornhaugland@gmail.com>
2020-01-23 15:16:52 +0100

    Change calculation of minutes,seconds,degrees Add tests to prove it

--- Dualog.eCatch.Shared.Tests/GeoAngleTests.cs
+++ Dualog.eCatch.Shared.Tests/GeoAngleTests.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using Dualog.eCatch.Shared.Models;
+using FluentAssertions;
 using Xunit;
 
 namespace Dualog.eCatch.Shared.Tests
@@ -9,16 +10,31 @@ namespace Dualog.eCatch.Shared.Tests
     public class GeoAngleTests
     {
         [Theory]
-        [InlineData(1.01, 1, 0)]
-        [InlineData(1.3, 1, 18)]
-        [InlineData(45.15, 45, 9)]
-        [InlineData(45.167, 45, 10)]
-        [InlineData(89.9999833333333, 89, 59)]
-        public void FromDouble_should_display_correct_values(double angleInDegrees, int expectedDegrees, int expectedMinutes)
+        [InlineData(1.01, 1, 0, 36)]
+        [InlineData(1.3, 1, 18, 0)]
+        [InlineData(45.15, 45, 9, 0)]
+        [InlineData(45.167, 45, 10, 1.2)]
+        [InlineData(89.9999833333333, 89, 59, 59.94)]
+        [InlineData(70.051326, 70, 3, 4.774)]
+        [InlineData(24.971782, 24, 58, 18.415)]
+
+        public void FromDouble_should_display_correct_minute_value(double angleInDegrees, int expectedDegrees, int expectedMinutes, double expectedSeconds)
         {
             var geoAngle = GeoAngle.FromDouble(angleInDegrees);
             Assert.Equal(expectedDegrees, geoAngle.Degrees);
             Assert.Equal(expectedMinutes, geoAngle.Minutes);
+            Assert.Equal(expectedSeconds, geoAngle.Seconds);
+        }
+
+        [Theory]
+        [InlineData(70.051326, 24.971782, "70° 03' 04.774\" N", "24° 58' 18.415\" E") ]
+        public void ToString_should_display_correct_value(double latInDegrees, double lonInDegrees, string expectedLat, string expectedLon)
+        {
+            var geoAngleLatString = GeoAngle.FromDouble(latInDegrees).ToString("NS");
+            var geoAngleLonString = GeoAngle.FromDouble(lonInDegrees).ToString("WE");
+            
+            geoAngleLatString.Should().Be(expectedLat);
+            geoAngleLonString.Should().Be(expectedLon);
         }
     }
 }
--- Dualog.eCatch.Shared/Models/GeoAngle.cs
+++ Dualog.eCatch.Shared/Models/GeoAngle.cs
@@ -8,7 +8,7 @@ namespace Dualog.eCatch.Shared.Models
         public bool IsNegative { get; set; }
         public int Degrees { get; set; }
         public int Minutes { get; set; }
-        public int Seconds { get; set; }
+        public double Seconds { get; set; }
         public int Milliseconds { get; set; }
         public double DecimalMinutes { get; set; }
 
@@ -18,42 +18,57 @@ namespace Dualog.eCatch.Shared.Models
             return negative ? res * -1 : res;
         }
 
+        /* PREVIOUS SOLUTION FOR CALCULATING FROM DECIMAL DEGREES TO MINUTESSECONDSDEGREES*/
+        //public static GeoAngle FromDouble(double angleInDegrees)
+        //{
+        //    //ensure the value will fall within the primary range [-180.0..+180.0]
+        //    while (angleInDegrees < -180.0)
+        //        angleInDegrees += 360.0;
+
+        //    while (angleInDegrees > 180.0)
+        //        angleInDegrees -= 360.0;
+
+        //    var result = new GeoAngle();
+
+        //    //switch the value to positive
+        //    result.IsNegative = angleInDegrees < 0;
+        //    angleInDegrees = Math.Abs(angleInDegrees);
+
+        //    //gets the degree
+        //    result.Degrees = (int)Math.Floor(angleInDegrees);
+        //    var delta = angleInDegrees - result.Degrees;
+        //    delta = Math.Round(delta, 3);
+        //    if (delta == 1)
+        //    {
+        //        delta = 0.999;
+        //    }
+
+        //    //gets minutes and seconds
+        //    var seconds = (int)Math.Floor(3600.0 * delta);
+        //    result.Seconds = seconds % 60;
+        //    result.Minutes = (int)Math.Floor(seconds / 60.0);
+        //    result.DecimalMinutes = seconds / 60.0;
+        //    delta = delta * 3600.0 - seconds;
+
+        //    //gets fractions
+        //    result.Milliseconds = (int)(1000.0 * delta);
+
+        //    return result;
+        //}
+
         public static GeoAngle FromDouble(double angleInDegrees)
         {
-            //ensure the value will fall within the primary range [-180.0..+180.0]
-            while (angleInDegrees < -180.0)
-                angleInDegrees += 360.0;
-
-            while (angleInDegrees > 180.0)
-                angleInDegrees -= 360.0;
-
+            var decimalDegrees = Math.Abs(angleInDegrees);
             var result = new GeoAngle();
 
-            //switch the value to positive
-            result.IsNegative = angleInDegrees < 0;
-            angleInDegrees = Math.Abs(angleInDegrees);
-
-            //gets the degree
-            result.Degrees = (int)Math.Floor(angleInDegrees);
-            var delta = angleInDegrees - result.Degrees;
-            delta = Math.Round(delta, 3);
-            if (delta == 1)
-            {
-                delta = 0.999;
-            }
-
-            //gets minutes and seconds
-            var seconds = (int)Math.Floor(3600.0 * delta);
-            result.Seconds = seconds % 60;
-            result.Minutes = (int)Math.Floor(seconds / 60.0);
-            result.DecimalMinutes = seconds / 60.0;
-            delta = delta * 3600.0 - seconds;
-
-            //gets fractions
-            result.Milliseconds = (int)(1000.0 * delta);
-
+            result.Degrees = (int)Math.Truncate(decimalDegrees);
+            result.Minutes = (int)Math.Truncate((decimalDegrees * 60) % 60);
+            result.Seconds = Math.Round(decimalDegrees * 3600 % 60, 3);
+            result.DecimalMinutes = 0;
+            result.Milliseconds = 0;
             return result;
         }
+
 
         public override string ToString()
         {
@@ -75,10 +90,10 @@ namespace Dualog.eCatch.Shared.Models
             {
                 case "NS":
                     //return $"{Degrees}° {Minutes:00}' {Seconds:00}\".{Milliseconds:000} {(IsNegative ? "SouthShort".Localize() : "NorthShort".Localize())}"; // TODO Localize
-                    return $"{Degrees}° {Minutes:00}' {Seconds:00}\".{Milliseconds:000} {(IsNegative ? "S" : "N")}";
+                    return $"{Degrees}° {Minutes:00}' {Seconds:00.000}\" {(IsNegative ? "S" : "N")}";
                 case "WE":
                     //return $"{Degrees}° {Minutes:00}' {Seconds:00}\".{Milliseconds:000} {(IsNegative ? "WestShort".Localize() : "EastShort".Localize())}"; // TODO Localize
-                    return $"{Degrees}° {Minutes:00}' {Seconds:00}\".{Milliseconds:000} {(IsNegative ? "W" : "E")}";
+                    return $"{Degrees}° {Minutes:00}' {Seconds:00.000}\" {(IsNegative ? "W" : "E")}";
                 default:
                     throw new NotImplementedException();
             }
