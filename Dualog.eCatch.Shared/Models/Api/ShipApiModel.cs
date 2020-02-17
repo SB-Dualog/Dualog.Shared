91e1663196dd39422a25042eac5ca893b16935e7
vebjornhaugland <vebjornhaugland@gmail.com>
2020-02-17 12:26:59 +0100

    Add ShipApiModel

---
+++ Dualog.eCatch.Shared/Models/Api/ShipApiModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Dualog.eCatch.Shared.Models.Api
+{
+    public class ShipApiModel
+    {
+        public string Name { get; set; }
+        public string RadioCallSignal { get; set; }
+        public string RegistrationNumber { get; set; }
+        public string VesselEmail { get; set; }
+    }
+}
