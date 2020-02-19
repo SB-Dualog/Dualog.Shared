8834c2b95b6c37af731fd8f309c9686d8583ecb5
Håvard K. Blixgård <havard.blixgard@dualog.com>
2020-02-19 11:07:30 +0100

    Added ActivateLinkDeviceRequest class

---
+++ Dualog.eCatch.Shared/Api/ActivateLinkDeviceRequest.cs
@@ -0,0 +1,23 @@
+﻿namespace Dualog.eCatch.Shared.Api
+{
+    public class ActivateLinkDeviceRequest
+    {
+        // Login
+        public string UserName { get; set; }
+        public string Password { get; set; }
+
+        // Device activation 
+        public string PublicKey { get; set; }
+        public string InstallTime { get; set; }
+        
+        // Ship data
+        public string ShipName { get; set; }
+        public string RadioCallSignal { get; set; }
+        public string RegistrationNumber { get; set; }
+        public string VesselEmail { get; set; }
+
+        // Device data
+        public string DeviceModel { get; set; }
+        public string DeviceSerial { get; set; }
+    }
+}
