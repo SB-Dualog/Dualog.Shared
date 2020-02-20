a80adc4e28f8b17ac4c23ba9a7a73a1c9a6505a7
Håvard K. Blixgård <47347838+havardix@users.noreply.github.com>
2020-02-20 09:54:06 +0100

    Link request response (#9)
    * Added ActivateLinkDeviceRequest class
    
    * Expanded ActivateLinkDeviceRequest class with more deviceinfo

---
+++ Dualog.eCatch.Shared/Api/ActivateLinkDeviceRequest.cs
@@ -0,0 +1,43 @@
+﻿namespace Dualog.eCatch.Shared.Api
+{
+    public class ActivateLinkDeviceRequest
+    {
+        // Login
+        public string LinkAuthToken { get; set; }
+
+        // Device activation 
+        public string PublicKey { get; set; }
+        public string InstallTime { get; set; }
+
+        // Ship data
+        public string ShipName { get; set; }
+        public string RadioCallSignal { get; set; }
+        public string RegistrationNumber { get; set; }
+        public string VesselEmail { get; set; }
+
+        // Device data
+        public string DeviceModel { get; set; }
+        public string DeviceSerial { get; set; }
+        public string DeviceVersion { get; set; }
+        public string AppVersion { get; set; }
+
+        public ActivateLinkDeviceRequest(
+            string linkAuthToken, 
+            string publicKey, string installTime, 
+            string shipName, string radioCallSignal, string registrationNumber, string vesselEmail, 
+            string deviceModel, string deviceSerial, string deviceVersion, string appVersion)
+        {
+            LinkAuthToken = linkAuthToken;
+            PublicKey = publicKey;
+            InstallTime = installTime;
+            ShipName = shipName;
+            RadioCallSignal = radioCallSignal;
+            RegistrationNumber = registrationNumber;
+            VesselEmail = vesselEmail;
+            DeviceModel = deviceModel;
+            DeviceSerial = deviceSerial;
+            DeviceVersion = deviceVersion;
+            AppVersion = appVersion;
+        }
+    }
+}
