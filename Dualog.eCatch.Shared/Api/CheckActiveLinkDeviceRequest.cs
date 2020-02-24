f855b35197bd5671b3448a43f60f3dbaa4d66053
Håvard K. Blixgård <47347838+havardix@users.noreply.github.com>
2020-02-24 15:08:45 +0100

    Added CheckActiveLinkDeviceRequest (#11)

---
+++ Dualog.eCatch.Shared/Api/CheckActiveLinkDeviceRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace Dualog.eCatch.Shared.Api
+{
+    public class CheckActiveLinkDeviceRequest
+    {
+        public string PublicKey { get; set; }
+        public string RadioCallSignal { get; set; }
+    }
+}
